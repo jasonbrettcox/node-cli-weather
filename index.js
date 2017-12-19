b254f0dcf4751d6b780211433378655c1f803947
jasonbcox <jasonbcox@gmail.com>
2017-12-19 15:48:27 -0700

    it works

--- index.js
+++ index.js
@@ -2,18 +2,26 @@
 
 
 //Require apiKey
-
+const secretKey = require('./env.js')
 //Require the request library
+const request = require('./node_modules/request/');
+
 //What is the `request` library you ask?
 //Google `npm request` and you can find out!
+
 
 
 // Set the API URL we are hitting, with our API Key and zipcode
 
 //The actual request sending
 
-request(apiUrl, function (error, response, body) {
+request('http://api.wunderground.com/api/' + secretKey + '/conditions/q/CO/Denver.json', function (error, response, body) {
   //Inside that callback
+let object = JSON.parse(body);
+let denverWeather = object.current_observation.weather
 
+  console.log("The weather in Denver is " + denverWeather + ".")
   //Print out the weather result
+
+
 });
